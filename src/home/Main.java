dee2bae81336dd74b4b240bb875cd15af85c3567
Olga <olga.molchanova.olkamana@mail.com>
2019-11-02 15:07:22 +0300

    findMin2
    New functions for dichotomy and golden section added

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,29 +1,28 @@
 package home;
 
-
-import static java.lang.Math.log;
-
 public class Main {
 
     static double a = 2;
     static double b = 200;
     static double epsilon = 0.001; //точность
     static int max_step = 10000;
-    static double delta = epsilon/3; //расстояние от середины отрезка
-    static double x; static int k;
+    static double delta = epsilon / 3; //расстояние от середины отрезка
+    static double x;
+    static int k;
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GoldenSection GS = new GoldenSection();
         System.out.println(GS.findMin(a, b, epsilon));
         Dichotomy D = new Dichotomy();
         System.out.println(D.findMin(a, b, epsilon, delta));
-
-
     }
 
 
-    public static double f(double x) { double f = Math.pow(( x - 15 ), 2) + 5; return f;}
+    public static double f(double x) {
+        double f = Math.pow((x - 15), 2) + 5;
+        return f;
+    }
 
 
 }
